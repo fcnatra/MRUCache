0c6c87d9b94d74246e95d372bb3ab6b8f089c5e4
Frank CNatra <fcnatra@yahoo.com>
2024-02-07 09:46:05 +0100

    more TESTS added to CacheSwapper

--- MruCachePOC/CacheSwapper/MemoryCacheSwapper.cs
+++ MruCachePOC/CacheSwapper/MemoryCacheSwapper.cs
@@ -3,7 +3,11 @@
 public class MemoryCacheSwapper : ICacheSwapper
 {
     public void Dump(Dictionary<object, object> entries, IEnumerable<object> keysToDump)
-    {}
+    {
+        foreach(var k in keysToDump)
+            entries.Remove(k);
+    }
+
     public bool Recover(Dictionary<object, object> entries, object key)
     {
         return true;
--- MruCachePOC/MruCache.Tests/CacheSwapperTests.cs
+++ MruCachePOC/MruCache.Tests/CacheSwapperTests.cs
@@ -7,18 +7,67 @@ namespace MruCache.Tests;
 public class CacheSwapperTests
 {
     [Fact]
+    public void GivenDumpedEntry_RecoverIt_ReturnsTrue()
+    {
+        ICacheSwapper cache = new MemoryCacheSwapper();
+
+        var nameOfKey1 = "key1";
+
+        var entries = new Dictionary<object, object>
+        {
+            { nameOfKey1, new MruCacheEntry<string>("value1") }
+        };
+        cache.Dump(entries, new List<object> {nameOfKey1});
+
+        // ACT
+        var wasRecovered = cache.Recover(entries, nameOfKey1);
+
+        // ASSERT
+        Assert.True(wasRecovered);
+    }
+
+    [Fact]
+    public void GivenThreeEntries_DumpingFirstTwo_LeavesThirdAloneInTheList()
+    {
+        ICacheSwapper cache = new MemoryCacheSwapper();
+
+        var nameOfKey3 = "key3";
+
+        var entries = new Dictionary<object, object>()
+        {
+            {"key1", new MruCacheEntry<string>("value1")},
+            {"key2", new MruCacheEntry<string>("value2")},
+            {nameOfKey3, new MruCacheEntry<string>("value3")}
+        };
+
+        // ACT
+        cache.Dump(entries, entries.Keys.Take(2));
+
+        // ASSERT
+        bool entryIsInTheList = entries.TryGetValue(nameOfKey3, out object? entry);
+
+        Assert.Equal(1, entries.Count());
+        Assert.True(entryIsInTheList);
+    }
+
+    [Fact]
     public void GivenTwoEntries_DumpingOne_RemovesEntryFromList()
     {
         ICacheSwapper cache = new MemoryCacheSwapper();
 
+        var nameOfKey2 = "key2";
+
         var entries = new Dictionary<object, object>();
         entries.Add("key1", new MruCacheEntry<string>("value1"));
-        entries.Add("key2", new MruCacheEntry<string>("value2"));
+        entries.Add(nameOfKey2, new MruCacheEntry<string>("value2"));
 
         // ACT
-        cache.Dump(entries, new List<object> {"value2"});
+        cache.Dump(entries, new List<object> {nameOfKey2});
 
         // ASSERT
+        bool entryIsInTheList = entries.TryGetValue(nameOfKey2, out object? entry);
+
         Assert.Equal(1, entries.Count());
+        Assert.False(entryIsInTheList);
     }
 }
